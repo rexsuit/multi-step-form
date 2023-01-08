b26287296441ba75830e52bb46bb8e6aa7092fda
Igor T <orshick@gmail.com>
2023-01-08 16:27:41 -0330

    more cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,29 @@ interface IFormInput {
 
 export default function App() {
   const { register, handleSubmit } = useForm<IFormInput>();
+
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
   const [step, setStep] = useState(0);
 
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello multi step form!</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         {step === 0 && (
           <TWInput label="First Name" {...register("firstName")} />
         )}
         {step === 1 && (
-          <TWInput label="middle name" {...register("middleName")} />
+          <TWInput label="Middle name" {...register("middleName")} />
         )}
         {step === 2 && <TWInput label="Last name" {...register("lastName")} />}
         <div className="flex flex-col gap-4">
-          <TWButton onClick={() => setStep(step + 1)}>next</TWButton>
-          <TWButton onClick={() => setStep(step - 1)}>prev</TWButton>
+          {step < 2 && (
+            <TWButton onClick={() => setStep(step + 1)}>next</TWButton>
+          )}
+          {step > 0 && (
+            <TWButton onClick={() => setStep(step - 1)}>prev</TWButton>
+          )}
           <TWButton type="submit">Submit</TWButton>
         </div>
       </form>
